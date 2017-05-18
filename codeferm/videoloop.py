b70c43aff8c431c775fda255f1fd3f46dd8639bd
sgoldsmith <sgoldsmith@dx4350>
2017-05-18 14:52:57 -0400

    Remove debug code

--- codeferm/videoloop.py
+++ codeferm/videoloop.py
@@ -270,7 +270,6 @@ class videoloop(observer.observer, observable.observable):
                     if skipCount <= 0:
                         skipCount = frameToCheck
                         resizeImg, grayImg, bwImg, motionPercent, movementLocationsFiltered = self.motion.detect(frame, timestamp)
-                        raise ValueError("forced exception")
                         if self.appConfig.historyImage and self.recording:
                             # Update history image
                             self.historyImg = numpy.bitwise_or(bwImg, self.historyImg)                    
